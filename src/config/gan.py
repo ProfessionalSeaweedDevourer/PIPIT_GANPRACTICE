5016c92cac902fd5f8967422338812d3afb273c7
ProfessionalSeaweedDevourer <164283978+ProfessionalSeaweedDevourer@users.noreply.github.com>
2024-08-23 14:48:25 +0900

    Multiple changes on gan.py
    Parameter changes

---
+++ src/config/gan.py
@@ -0,0 +1,35 @@
+#epochs: int = 100
+#epochs: int = 200
+#epochs: int = 300
+#epochs: int = 1000
+#epochs: int = 500
+epochs: int = 750
+
+#d_loops: int = 2
+#g_loops: int = 2
+#d_loops: int = 4
+#g_loops: int = 4
+#d_loops: int = 5
+#d_loops: int = 6
+d_loops: int = 3
+g_loops: int = 7
+
+#d_lr: float = 1e-4
+#g_lr: float = 1e-4
+#d_lr: float = 2e-4
+#g_lr: float = 2e-4
+d_lr: float = 1.25e-4
+g_lr: float = 2.5e-4
+
+
+
+#hl_lambda = 1e-5
+#hl_lambda = 1e-4
+#hl_lambda = 5e-5
+hl_lambda = 2e-5
+
+#wgan_clamp = [-0.1, 0.1]
+#wgan_clamp = [-0.05, 0.05]
+wgan_clamp = [-0.075, 0.075]
+#wgangp_lambda = 10
+wgangp_lambda = 5
